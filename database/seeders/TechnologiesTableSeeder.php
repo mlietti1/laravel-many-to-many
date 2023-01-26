92dd1add207989ed0baf0efcc0c051bb919530d5
mlietti1 <mcf.lietti@outlook.com>
2023-01-26 18:11:32 +0100

    migrations, seeders and pivot table

---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=0; $i<200;$i++){
+            $project = Project::inRandomOrder()->first();
+            $technology_id = Technology::inRandomOrder()->first()->id;
+            $project->technologies()->attach($technology_id);
+        }
+    }
+}
